525f07f54482cf08cf0aa6e302e136a8538b8b5b
AryanMittal2603 <71939548+AryanMittal2603@users.noreply.github.com>
2024-09-25 15:37:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,11 @@ import numpy as np
 
 dataframe = np.random.randn(5, 5)
 st.dataframe(dataframe)
+
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
