45e245ab4d6a3a49e8afe3ddb7844ef20323c875
AryanMittal2603 <71939548+AryanMittal2603@users.noreply.github.com>
2024-09-25 15:39:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,12 @@ from pathlib import Path
 import streamlit as st
 import numpy as np
 
+st.write("Printing Streamlit Dataframe (st.dataframe) :")
+
 dataframe = np.random.randn(5, 5)
 st.dataframe(dataframe)
 
-
+st.write("Printing pandas directly:")
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
