edcf5d910fc20c7dc2801dfe0c54a499bba2f2bb
AryanMittal2603 <71939548+AryanMittal2603@users.noreply.github.com>
2024-09-25 16:10:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,13 @@ from pathlib import Path
 import streamlit as st
 import numpy as np
 
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-    })
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
 
-option = st.selectbox(
-    'Which number do you like best?',
-     df['second column'])
-
-'You selected: ', option
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
