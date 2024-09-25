db3bfeb4dbf76785ec70d57d5086147c16afbf8f
AryanMittal2603 <71939548+AryanMittal2603@users.noreply.github.com>
2024-09-25 15:55:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import numpy as np
 
 
 map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    np.random.randn(10, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
 
 st.map(map_data)
