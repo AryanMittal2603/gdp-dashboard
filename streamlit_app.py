3221037147a4ed9bfdf91a3e860781e59fa28a95
AryanMittal2603 <71939548+AryanMittal2603@users.noreply.github.com>
2024-09-25 15:42:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,10 @@ from pathlib import Path
 import streamlit as st
 import numpy as np
 
-st.write("Printing Streamlit Dataframe (st.dataframe) :")
 
-dataframe = np.random.randn(5, 5)
-st.dataframe(dataframe)
 
-st.write("Printing pandas directly:")
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
 
-df
+st.dataframe(dataframe.style.highlight_max(axis=0))
